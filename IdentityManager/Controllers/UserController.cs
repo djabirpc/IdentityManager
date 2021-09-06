b899525ca9afb01442e80730918a4095c0886e05
djabirpc <djabir.pc@outlook.fr>
2021-09-06 16:22:52 +0100

    Users Management

--- IdentityManager/Controllers/UserController.cs
+++ IdentityManager/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using IdentityManager.Data;
+using IdentityManager.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,6 +41,114 @@ namespace IdentityManager.Controllers
             }
 
             return View(userList);
+        }
+        
+        [HttpGet]
+        public IActionResult Edit(string userId)
+        {
+            //getting the user
+            var objFromDb = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+            if(objFromDb == null)
+            {
+                return NotFound();
+            }
+            //getting all assotionation with user role and user tables
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            //see if any role that user have
+            var role = userRole.FirstOrDefault(u => u.UserId == objFromDb.Id);
+            if(role != null)
+            {
+                objFromDb.RoleId = roles.FirstOrDefault(u => u.Id == role.RoleId).Id;
+            }
+
+            objFromDb.RoleList = _db.Roles.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+
+            return View(objFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ApplicationUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                //getting the user
+                var objFromDb = _db.ApplicationUser.FirstOrDefault(u => u.Id == user.Id);
+                if (objFromDb == null)
+                {
+                    return NotFound();
+                }
+                //getting all assotionation with user role and user tables
+                var userRole = _db.UserRoles.FirstOrDefault(u => u.UserId == objFromDb.Id);
+
+                //already have prev Roles and we will add the new role to it
+                if (userRole != null)
+                {
+                    //take the name of prev role
+                    var previousRoleName = _db.Roles.Where(u => u.Id == userRole.RoleId).Select(e => e.Name).FirstOrDefault();
+                    //remove old role
+                    await _userManager.RemoveFromRoleAsync(objFromDb, previousRoleName);
+
+                }
+                //add new role
+                await _userManager.AddToRoleAsync(objFromDb, _db.Roles.FirstOrDefault(u => u.Id == user.RoleId).Name);
+                objFromDb.Name = user.Name;
+                _db.SaveChanges();
+                TempData[SD.Success] = "User have been updated!";
+                return RedirectToAction(nameof(Index));
+            }
+
+            user.RoleList = _db.Roles.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+            return View(user);
+        }
+    
+        [HttpPost]
+        public IActionResult LockUnlock(string userId)
+        {
+            var objFromDb = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+            if(objFromDb == null)
+            {
+                return NotFound();
+            }
+            if(objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                //user is locked and will remain locked untill lockoutend time
+                //clikcing on this action will unlock them
+                objFromDb.LockoutEnd = DateTime.Now;
+                TempData[SD.Success] = "User unlocked successfully";
+            }
+            else
+            {
+                //user is np locked, and we want to lock the user
+                objFromDb.LockoutEnd = DateTime.Now.AddMinutes(15);
+                TempData[SD.Success] = "User locked successfully";
+            }
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string userId)
+        {
+            var objFromDb = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+            if(objFromDb == null)
+            {
+                return NotFound();
+            }
+            _db.ApplicationUser.Remove(objFromDb);
+            _db.SaveChanges();
+            TempData[SD.Success] = "User deleted!";
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
